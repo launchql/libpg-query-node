dc4f65e5ba7a2dd64c328d4882cd1bef684ce532
Zac McCormick <zac.mccormick@gmail.com>
2016-05-16 10:23:23 -0400

    Update to the newer libpg_query

--- test/index.js
+++ test/index.js
@@ -3,15 +3,15 @@ var assert = require('assert');
 
 describe('pg-query', function() {
   it('should parse a query', function() {
-    assert.equal(typeof query.parse('select 1').query[0].SELECT, 'object');
+    assert.equal(typeof query.parse('select 1').query[0].SelectStmt, 'object');
   });
 
   it('should parse a null', function() {
-    assert(query.parse("select null").query[0].SELECT.targetList[0].RESTARGET.val.A_CONST.val === null);
+    assert(query.parse("select null").query[0].SelectStmt.targetList[0].ResTarget.val.A_Const.val.Null);
   });
 
   it('should parse an empty string', function() {
-    assert(query.parse("select ''").query[0].SELECT.targetList[0].RESTARGET.val.A_CONST.val === '');
+    assert(query.parse("select ''").query[0].SelectStmt.targetList[0].ResTarget.val.A_Const.val.String.str === '');
   });
 
   it('should not parse a bogus query', function() {
