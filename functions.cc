ce3b892d02a8b4842aa95b2b0c89082b5a12135d
Zac McCormick <zac.mccormick@gmail.com>
2016-06-03 10:18:43 -0400

    Upgrade libpg_query

--- functions.cc
+++ functions.cc
@@ -30,13 +30,22 @@ NAN_METHOD(parse) {
 
     v8::Local<v8::String> message = Nan::New(result.error->message).ToLocalChecked();
     v8::Local<v8::String> fileName = Nan::New(result.error->filename).ToLocalChecked();
+    v8::Local<v8::String> functionName = Nan::New(result.error->funcname).ToLocalChecked();
     v8::Local<v8::Number> lineNumber = Nan::New(result.error->lineno);
     v8::Local<v8::Number> cursorPosition = Nan::New(result.error->cursorpos);
 
     Nan::Set(error, Nan::New("message").ToLocalChecked(), message);
     Nan::Set(error, Nan::New("fileName").ToLocalChecked(), fileName);
+    Nan::Set(error, Nan::New("functionName").ToLocalChecked(), functionName);
     Nan::Set(error, Nan::New("lineNumber").ToLocalChecked(), lineNumber);
     Nan::Set(error, Nan::New("cursorPosition").ToLocalChecked(), cursorPosition);
+
+    if (result.error->context) {
+      Nan::Set(error, Nan::New("context").ToLocalChecked(), Nan::New(result.error->context).ToLocalChecked());
+    }
+    else {
+      Nan::Set(error, Nan::New("context").ToLocalChecked(), Nan::Null());
+    }
 
     Nan::Set(hash, Nan::New("error").ToLocalChecked(), error);
   }
