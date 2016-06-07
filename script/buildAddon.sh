2894741bdf1f6a51e68352acbecd36fba11f2045
matus kislan <matuskislan@gmail.com>
2016-06-07 15:04:34 +0200

    changed to preinstall

--- script/buildAddon.sh
+++ script/buildAddon.sh
@@ -5,7 +5,7 @@ tmpDir=tmp_pg
 
 mkdir -p $tmpDir
 cd $tmpDir
-git clone git://github.com/lfittl/libpg_query
+git clone https://github.com/lfittl/libpg_query
 
 cd libpg_query
 make
@@ -25,53 +25,7 @@ fi
 
 cp $(pwd)/libpg_query.a $rDIR/libpg_query/linux/
 
-cd $rDIR
 
-node-gyp configure
-
-node-gyp build
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: node-gyp failed to build pg-query.node";
-	exit 1;
-fi
-
-echo "node-gyp successfully built pg-query.node";
-
-if [ ! -d $rDIR/build ]; then
-    echo "ERROR: $rDIR/build not found";
-    exit 1;
-fi
-
-cd build/
-
-if [ ! -d $rDIR/build/Release ]; then
-	echo "ERROR: $rDIR/build/Release not found";
-	exit 1;
-fi
-
-cd Release/
-
-if [ ! -f $rDIR/build/Release/pg-query.node ]; then
-	echo "ERROR: $rDIR/build/Release/pg-query.node not found";
-	exit 1;
-fi
-
-mkdir -p $rDIR/libs
-
-cp $rDIR/build/Release/pg-query.node $rDIR/libs
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: failed to move pg-query.node to libs directory";
-	exit 1;
-fi
-
-echo "pg-query.node copied to libs directory";
-
-rm -rf $rDIR/$tmpDir
-rm -rf $rDIR/build
-
-echo "Successfully built pg-query.node";
 
 
 
