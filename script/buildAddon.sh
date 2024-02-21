30ab7da236a7d79f234895f290a0b194e4430f76
Dan Lynch <pyramation@gmail.com>
2024-02-21 03:55:19 +0100

    readme, addon

--- script/buildAddon.sh
+++ script/buildAddon.sh
@@ -1,58 +1,67 @@
 #!/usr/bin/env bash
 
-commit=95c1f0b43b71123b35956c4a19806c2f70d31e0e
+# Set the desired commit hash and branch
+commit=e7c14fe399c4903223362790c212bcbeb6832864
+branch=13-latest
 
+# Remember current directory and create a new, unique, temporary directory
 rDIR=$(pwd)
 tmpDir=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir.XXXX')
+
+# Define the make target
 makeTarget=build
 
+# Change to the newly created temp directory
 cd "$tmpDir"
 
-git clone -b 13-latest --single-branch https://github.com/pganalyze/libpg_query.git
+# Clone the selected branch of the libpg_query Git repo
+git clone -b $branch --single-branch https://github.com/pganalyze/libpg_query.git
 cd libpg_query
 
+# Checkout the desired commit
 git checkout $commit
 
 # needed if being invoked from within gyp
 unset MAKEFLAGS
 unset MFLAGS
 
+# Adaptively build for macOS or Linux
 if [ "$(uname)" == "Darwin" ]; then
 	make CFLAGS='-mmacosx-version-min=10.7' PG_CFLAGS='-mmacosx-version-min=10.7' $makeTarget
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	make CFLAGS='' PG_CFLAGS='' $makeTarget
 fi
 
+# Terminate if build fails
 if [ $? -ne 0 ]; then
 	echo "ERROR: 'make' command failed";
 	exit 1;
 fi
 
+# Search for libpg_query.a, error if not found
 file=$(ls | grep 'libpg_query.a')
-
 if [ ! $file ]; then
 	echo "ERROR: libpg_query.a not found";
 	exit 1;
 fi
 
+# Error if pg_query.h is missing
 file=$(ls | grep 'pg_query.h')
-
 if [ ! $file ]; then
 	echo "ERROR: pg_query.h not found";
 	exit 1;
 fi
 
-#copy queryparser.cc, binding.gyp to current directory
-#
-#
-
+# Copy queryparser.cc, binding.gyp to current directory
 if [ "$(uname)" == "Darwin" ]; then
     cp $(pwd)/libpg_query.a $rDIR/libpg_query/osx/
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cp $(pwd)/libpg_query.a $rDIR/libpg_query/linux/
 fi
 
+# Copy header
 cp $(pwd)/pg_query.h $rDIR/libpg_query/include/
 
+# Cleanup: revert to original directory and remove the temp
 cd "$rDIR"
 rm -rf "$tmpDir"
