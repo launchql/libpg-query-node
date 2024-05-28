3700cc7632b965fc7da408272ffb5ee1829c512a
Dan Lynch <pyramation@gmail.com>
2024-05-28 21:10:18 +0200

    add types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
-export function parseQuery(sql: string): Promise<any>;
+import { ParseResult } from "@pgsql/types";
+export function parseQuery(sql: string): Promise<ParseResult>;
 export function parsePlPgSQL(funcsSql: string): Promise<any>;
-export function parseQuerySync(sql: string): any;
+export function parseQuerySync(sql: string): ParseResult;
 export function parsePlPgSQLSync(funcsSql: string): any;
 export function fingerprint(sql: string): Promise<string>;
 export function fingerprintSync(sql: string): string;
