3a9f74891f3a4d3a4eb9c605800d9c9767b0d161
Dan Lynch <pyramation@gmail.com>
2025-06-16 14:29:21 -0700

    cleanup entry point

--- wasm/index.d.ts
+++ wasm/index.d.ts
@@ -1,16 +1,18 @@
 import { ParseResult } from "@pgsql/types";
 
+export async function loadModule(): Promise<void>;
+
 export function parseQuery(sql: string): Promise<ParseResult>;
 export function parsePlPgSQL(funcsSql: string): Promise<any>;
 export function parseQuerySync(sql: string): ParseResult;
 export function parsePlPgSQLSync(funcsSql: string): any;
 export function deparse(parseTree: any): Promise<string>;
-export function deparseSync(parseTree: any): any;
+export function deparseSync(parseTree: any): string;
 export function fingerprint(sql: string): Promise<string>;
 export function fingerprintSync(sql: string): string;
 export function normalize(sql: string): Promise<string>;
 export function normalizeSync(sql: string): string;
 export function parseQueryDetailed(sql: string): Promise<ParseResult>;
 export function parseQueryDetailedSync(sql: string): ParseResult;
-export function isReady(): boolean;
-export * from '@pgsql/types';
+
+export * from '@pgsql/types'; 
